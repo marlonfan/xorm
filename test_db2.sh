77bfbe0d8f5c0d3a9ed2cd6d8929a1bb832b6d75
Lunny Xiao <xiaolunwen@gmail.com>
2019-10-23 16:29:00 +0800

    upgrade db2 driver version

--- test_db2.sh
+++ test_db2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-go get github.com/ibmdb/go_ibm_db
+GO111MODULE=off go get github.com/ibmdb/go_ibm_db
 export DB2HOME=$GOPATH/src/github.com/ibmdb/go_ibm_db/installer
 cur="$PWD"
-cd $DB2HOME && go run setup.go
+GO111MODULE=off cd $DB2HOME && go run setup.go
 export CGO_CFLAGS=-I$DB2HOME/include
 export CGO_LDFLAGS=-L$DB2HOME/lib
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$DB2HOME/clidriver/lib
